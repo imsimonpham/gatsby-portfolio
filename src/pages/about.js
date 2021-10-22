cfc174511a6d4b30b1b3343df3d280b37ac52756
Simon <simonpham1294@gmail.com>
2021-10-22 11:34:26 -0300

    first-commit

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+          reprehenderit voluptas consectetur labore, est a vel maiores incidunt
+          perspiciatis iste aut eaque cumque consequatur quae corrupti,
+          provident nostrum quasi eum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+          reprehenderit voluptas consectetur labore, est a vel maiores incidunt
+          perspiciatis iste aut eaque cumque consequatur quae corrupti,
+          provident nostrum quasi eum?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+          reprehenderit voluptas consectetur labore, est a vel maiores incidunt
+          perspiciatis iste aut eaque cumque consequatur quae corrupti,
+          provident nostrum quasi eum?
+        </p>
+      </div>
+    </Layout>
+  )
+}
---
+++ src/pages/projects.js
@@ -0,0 +1,71 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/Layout"
+import * as styles from "../styles/projects.module.css"
+import Img from "gatsby-image"
+
+export default function Projects({ data }) {
+  console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
+
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Portfolio</h2>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>
+          {projects.map(project => {
+            return (
+              <Link
+                to={"/projects/" + project.frontmatter.slug}
+                key={project.id}
+              >
+                <div>
+                  <Img
+                    fluid={project.frontmatter.thumb.childImageSharp.fluid}
+                  />
+
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            )
+          })}
+        </div>
+        <p>Like what you see? Email me at {contact} for a quote</p>
+      </div>
+    </Layout>
+  )
+}
+
+//export page query
+
+export const query = graphql`
+  query ProjectsPage {
+    projects: allMarkdownRemark(
+      sort: { order: DESC, fields: frontmatter___date }
+    ) {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+    contact: site {
+      siteMetadata {
+        contact
+      }
+    }
+  }
+`
